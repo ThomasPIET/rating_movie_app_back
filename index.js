49a5fdb430264ad90a75a73c3f29090107e89832
PIET Thomas <thomas.piet@ynov.com>
2024-12-09 15:10:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const app = require('./server');
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+app.get('/search', (req, res) => {
+    const query = req.query.query;
+    console.log(`You searched for: ${query}`);
+
+    const results = [
+        { id: 1, name: `Movie 1` },
+        { id: 2, name: `Movie 2` },
+        { id: 3, name: `Inception` },
+    ];
+
+    const filteredResults = results.filter((result) => {
+        return result.name.toLowerCase().includes(query.toLowerCase());
+
+    });
+
+    res.json(filteredResults);
+});

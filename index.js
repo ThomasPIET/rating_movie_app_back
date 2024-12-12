7e75bee050e47417c6175b53a94aeb1a7ca0e2ae
PIET Thomas <thomas.piet@ynov.com>
2024-12-12 15:16:13 +0100

    add popular data

--- index.js
+++ index.js
@@ -17,20 +17,44 @@ app.get('/search', async (req, res) => {
                 query: query,
                 language: 'fr-FR',
                 page: 1,
+                sort_by: 'popularity.desc'
             }
         });
 
         const results = response.data.results;
 
         if (results.length === 0) {
-            return res.status(404).json({ message: 'Nothing found.' });
+            return res.status(404).json({message: 'Nothing found.'});
         }
 
         return res.json(results);
     } catch (error) {
         console.error('Error while fetching data from TMDb:', error);
         if (!res.headersSent) {
-            return res.status(500).json({ error: 'Error while fetching data from TMDb' });
+            return res.status(500).json({error: 'Error while fetching data from TMDb'});
+        }
+    }
+});
+
+app.get('/popular', async (req, res) => {
+    console.log('You requested popular movies.');
+    try {
+        const response = await axios.get('https://api.themoviedb.org/3/discover/movie', {
+            params: {
+                api_key: process.env.TMDB_API_KEY,
+                language: 'fr-FR',
+                page: 1,
+
+            }
+        });
+
+        const results = response.data.results;
+
+        return res.json(results);
+    } catch (error) {
+        console.error('Error while fetching data from TMDb:', error);
+        if (!res.headersSent) {
+            return res.status(500).json({error: 'Error while fetching data from TMDb'});
         }
     }
 });
